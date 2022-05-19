4f1f76c0be49269a26724d1c99769b714e106f69
JeTSkY1h <jetsky1h@googlemail.com>
2022-05-19 16:58:22 +0200

    added Failsafe for 0 students

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -6,6 +6,7 @@ public class Arrays {
     }
 
     public static int ArraySum(int[] nArr) {
+    
         int[] resArr = ArraySumInternal(nArr);
         return resArr[0];
     }
@@ -14,12 +15,16 @@ public class Arrays {
         if (nArr.length == 1) {
             return nArr;
         }
+
         int[] newArr = java.util.Arrays.copyOf(nArr, nArr.length - 1);
         newArr[newArr.length - 1] = nArr[nArr.length - 1] + nArr[nArr.length- 2];
         return ArraySumInternal(newArr);
     }
 
     public static String[] studentsMethod(int n) {
+        if (n < 0) {
+            return new String[0];
+        }
         String[] students = new String[n];
         for (int i = 0; i < n; i++) {
             students[i] = "Student " + i;
