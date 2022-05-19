e3a0bc3ed7b0307fdd9cf6296120ec1a3ab597c3
JeTSkY1h <jetsky1h@googlemail.com>
2022-05-19 14:31:16 +0200

    Added Array Tasks

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,29 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] nArr = {1,2,3,4};
+        int test = ArraySum(nArr);
+        System.out.println(test);
+    }
+
+    public static int ArraySum(int[] nArr) {
+        int[] resArr = ArraySumInternal(nArr);
+        return resArr[0];
+    }
+
+    public static int[] ArraySumInternal(int[] nArr) {
+        if (nArr.length == 1) {
+            return nArr;
+        }
+        int[] newArr = java.util.Arrays.copyOf(nArr, nArr.length - 1);
+        newArr[newArr.length - 1] = nArr[nArr.length - 1] + nArr[nArr.length- 2];
+        return ArraySumInternal(newArr);
+    }
+
+    public static String[] studentsMethod(int n) {
+        String[] students = new String[n];
+        for (int i = 0; i < n; i++) {
+            students[i] = "Student " + i;
+        }
+        return students;
+    }
+}
---
+++ src/test/java/ArraysTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArraysTest {
+    @Test
+    void shouldReturnArrayWith10Students() {
+        //given
+        int n = 10;
+        //then
+        var res = Arrays.studentsMethod(n);
+        //when
+        String[] expected = {
+            "Student 0",
+            "Student 1",
+            "Student 2",
+            "Student 3",
+            "Student 4",
+            "Student 5",
+            "Student 6",
+            "Student 7",
+            "Student 8",
+            "Student 9",
+        };
+
+        assertArrayEquals(expected, res);
+        
+    }
+
+    @Test
+    void shouldReturnArrayWith1Students() {
+        //given
+        int n = 1;
+        //then
+        var res = Arrays.studentsMethod(n);
+        //when
+        String[] expected = {
+            "Student 0",
+        };
+
+        assertArrayEquals(expected, res);
+        
+    }
+
+    @Test 
+    void shouldReturn10(){
+        //given
+        int[] nArr = {1,2,3,4};
+        //then
+        var res = Arrays.ArraySum(nArr);
+        //when
+        assertEquals(10, res);
+    }
+
+    @Test
+    void shouldReturn39(){
+        //given
+        int[] nArr = {10,10,11,8};
+        //then
+        var res = Arrays.ArraySum(nArr);
+        //when
+        assertEquals(39, res);
+    }
+}
